a9038b64c8a869f7fc8d89d599cf6b437f229fea
hackertaco <imsa1224@naver.com>
2020-11-09 00:29:25 +0900

    feat: ⚡️main setting

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ import routes from "./routes/routes";
 import MaterialDashboard from "./material-dashboard";
 import Chartist from "chartist";
 import GlobalComponents from "./globalComponents";
-import Notifications from "./components/NotificationPlugin";
+import GlobalDirectives from './globalDirectives'
+// import Notifications from "./components/NotificationPlugin";
 
 const router = new VueRouter({
   routes, // short for routes: routes
@@ -16,8 +17,8 @@ const router = new VueRouter({
 Vue.use(VueRouter);
 Vue.use(MaterialDashboard);
 Vue.use(GlobalComponents);
-// Vue.use(GlobalDirectives);
-Vue.use(Notifications);
+Vue.use(GlobalDirectives);
+// Vue.use(Notifications);
 
 Vue.prototype.$Chartist = Chartist;
 Vue.config.productionTip = false

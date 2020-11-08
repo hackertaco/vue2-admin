1283a14a889df4b078d29d78ca5882882577eadf
hackertaco <imsa1224@naver.com>
2020-11-09 00:28:48 +0900

    feat: ⚡️dropdown, topnavbar, chartcard

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="dropdown" :class="{open:isOpen}" @click="toggleDropDown" v-click-outside="closeDropDown">
+    <slot name="title">
+        <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown">
+            <i :class="icon"></i>
+            <p class="notification">{{title}}
+                <b class="caret"></b>
+            </p>
+        </a>
+    </slot>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'drop-down',
+    props: {
+        title: String,
+        icon: String
+    },
+    data() {
+        return {
+            isOpen: false
+        }
+    },
+    methods: {
+        toggleDropDown() {
+            this.isOpen = !this.isOpen;
+        },
+        closeDropDown() {
+            this.isOpen = false;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

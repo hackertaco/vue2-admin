18ecde1a751e5e88b1606217f05e8815fe5ff78f
hackertaco <imsa1224@naver.com>
2020-11-08 17:40:45 +0900

    feat: ⚡basic set

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
+import VueRouter from "vue-router";
 import App from './App.vue'
 
+import routes from "./routes/routes";
+import MaterialDashboard from "./material-dashboard";
+import Chartist from "chartist";
+import GlobalComponents from "./globalComponents";
+import Notifications from "./components/NotificationPlugin";
+
+const router = new VueRouter({
+  routes, // short for routes: routes
+  linkExactActiveClass: "nav-item active"
+});
+
+Vue.use(VueRouter);
+Vue.use(MaterialDashboard);
+Vue.use(GlobalComponents);
+// Vue.use(GlobalDirectives);
+Vue.use(Notifications);
+
+Vue.prototype.$Chartist = Chartist;
 Vue.config.productionTip = false
 
 new Vue({
+  el: "#app",
   render: h => h(App),
-}).$mount('#app')
+  router,
+  data: {
+    Chartist: Chartist
+  }
+});
